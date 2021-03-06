f2021ed25ea5e59ac65e395e1b0842b90dc45719
Imran Nazir <imran.nazir@yahoo.co.uk>
2021-03-06 00:43:11 +0000

    working state

--- src/View/View.ts
+++ src/View/View.ts
@@ -1,13 +1,40 @@
+// @ts-ignore
+import { Prompt, Select } from "enquirer";
+import Mediator from "../Mediator/Mediator";
 import { BaseComponent } from "../types";
 
 class View extends BaseComponent {
-  selectFile(files: string[]) {
-    if (!this.mediator) throw new Error("View: No mediator set");
+  mediator: Mediator | null = null;
+  fileNames: string[] = [];
+  prompt: Prompt | null = null;
 
-    this.mediator.request({
-      action: "view:select-file",
-      data: { selection: "testFile2" },
+  promptUser(list: string[]) {
+    if (!this.mediator) throw new Error("View: No mediator set");
+    if (list.length === 0) throw new Error("No files found");
+    this.fileNames = list;
+    this.prompt = new Select({
+      message: "Select a csv file",
+      choices: list,
     });
+    // this.prompt?.addListener('submit', and)
+  }
+
+  submit(selection: string) {
+    this.mediator?.request({
+      action: "view:select-file",
+      data: { selection: selection },
+    });
+  }
+  selectChoice() {
+    this.prompt
+      ?.run()
+      .then((selection) => {
+        this.mediator?.request({
+          action: "view:select-file",
+          data: { selection: selection },
+        });
+      })
+      .catch(console.error);
   }
 }
 
--- src/View/__tests__/View.test.ts
+++ src/View/__tests__/View.test.ts
@@ -4,19 +4,44 @@ import View from "../View";
 const view = new View();
 const mediator = new Mediator({ view });
 const mockRequest = jest.spyOn(mediator, "request");
+const stdin = require("mock-stdin").stdin();
 
 describe("view", () => {
-  it("should request a chosen file to be read", () => {
+  it("should request a chosen file to be read", (done) => {
     const testFile1 = "testFile1";
     const testFile2 = "testFile2";
     const fileNames = [testFile1, testFile2];
 
-    view.selectFile(fileNames);
-
-    expect(mockRequest).toHaveBeenCalledWith({
-      action: "view:select-file",
-      data: { selection: testFile2 },
+    // sets up the user prompt
+    view.promptUser(fileNames);
+    view.selectChoice();
+    // select the file to read
+    view.prompt?.on("run", () => {
+      // process.nextTick(() => {
+      stdin.send(`\u001b[B`);
+      stdin.send(`\u000d`);
+      // });
+      // expect(mockRequest).toHaveBeenCalledWith({
+      //   action: "view:select-file",
+      //   data: { selection: testFile2 },
+      // });
+      // done();
     });
+    view.prompt?.on("submit", (selection) => {
+      console.log("SUBMISSIONS", selection);
+      view.submit(selection);
+      // process.nextTick(() => {
+      //   stdin.send(`\u001b[B`);
+      //   stdin.send(`\u000d`);
+      // });
+      expect(mockRequest).toHaveBeenCalledWith({
+        action: "view:select-file",
+        data: { selection: testFile2 },
+      });
+      done();
+    });
+
+    // assert
   });
   it.skip("should emit a skip action when user triggered", () => {});
   it.skip("should emit an exit action when user triggered", () => {});
