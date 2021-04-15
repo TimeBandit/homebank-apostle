987bab561acadde898f4ef4c6065b1fc3b1c40a6
Imran Nazir <imran.nazir@yahoo.co.uk>
2021-04-15 22:01:20 +0100

    parser parse part complete

--- src/parser/__tests__/parser.test.ts
+++ src/parser/__tests__/parser.test.ts
@@ -3,24 +3,24 @@ import smilebankStrategy from "../strategies/smilebank-strategy";
 
 const testData = [
   [
-    "2018-10-31,LINK    15:37OCT31,ATM,,250.00,2374.65",
-    "2018-10-31;;;;LINK    15:37OCT31;-250;;ATM",
+    "2019-05-28,LINK    09:14MAY25,ATM,,20.00,2254.25",
+    "2019-05-28;3;;;LINK    09:14MAY25;-20;;",
   ],
   [
-    "2018-10-25,E HERRERO-FOS      ROOM RENT,CREDIT,350.00,,3511.18",
-    "2018-10-25;;;;E HERRERO-FOS      ROOM RENT;350;;CREDIT",
+    "2019-09-04,JUSTPARK WITHDRAWA 377824,CREDIT,855.20,,4378.24",
+    "2019-09-04;8;;;JUSTPARK WITHDRAWA 377824;855.2;;",
   ],
   [
-    "2018-10-31,VIRGIN MOBILE      FA63305629,DD,,11.50,2363.15",
-    "2018-10-31;;;;VIRGIN MOBILE      FA63305629;-11.5;;DD",
+    "2019-09-02,VIRGIN MOBILE      FA63305629,DD,,40.77,3523.04",
+    "2019-09-02;11;;;VIRGIN MOBILE      FA63305629;-40.77;;",
   ],
   [
-    "2018-11-02,ASDA SUPERSTORE,PURCHASE,,64.05,2251.36",
-    "2018-11-02;;;;ASDA SUPERSTORE;-64.05;;PURCHASE",
+    "2019-05-29,TESCO STORE 2813,PURCHASE,,2.71,2626.54",
+    "2019-05-29;6;;;TESCO STORE 2813;-2.71;;",
   ],
   [
-    "2018-10-25,SABINE-NW-ACC      PAY BACK FOR INTER,BP/SO,,1050.00,3161.18",
-    "2018-10-25;;;;SABINE-NW-ACC      PAY BACK FOR INTER;-1050;;BP/SO",
+    "2019-08-30,CRABB CURTIS       W1529 PATTERSON,BP/SO,,750.00,3563.81",
+    "2019-08-30;7;;;CRABB CURTIS       W1529 PATTERSON;-750;;",
   ],
   [
     "2018-11-15,TFR 77915682212360,TRANSFER,,150.00,1829.51",
@@ -28,14 +28,14 @@ const testData = [
   ],
   [
     "2018-11-15,TFR 77915682212360,UNKNOWN,,150.00,1829.51",
-    "2018-11-15;4;;;TFR 77915682212360;-150;;",
+    "2018-11-15;0;;;TFR 77915682212360;-150;;",
   ],
 ];
 
 const testParser = new Parser();
 testParser.setParser(smilebankStrategy);
 describe("parser", () => {
-  test.each(testData)("give %s parser should return %s", (source, parsed) => {
+  test.each(testData)("given %s parser should return %s", (source, parsed) => {
     const result = testParser.parse(source);
     expect(result).toEqual(parsed);
   });
--- src/parser/parser.ts
+++ src/parser/parser.ts
@@ -4,8 +4,9 @@ import BaseStrategy from "./strategies/base-strategy";
 class Parser extends BaseComponent {
   private _parser: BaseStrategy | null = null;
 
-  parse(lineOfCsv: string) {
-    this._parser?.parse(lineOfCsv);
+  parse(lineOfCsv: string): string {
+    if (!this._parser) throw new Error("no parser strategy found");
+    return this._parser?.parse(lineOfCsv);
   }
 
   setParser(strategy: BaseStrategy) {
